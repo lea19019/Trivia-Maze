6a4d17f3f6bcca20db0e8216eca6bc633827b0f7
Torin Bolander <bol19001@byui.edu>
2021-05-24 15:33:25 -0400

    Forgot the update method

--- triviaMaze.py
+++ triviaMaze.py
@@ -1,24 +1,31 @@
 import arcade
 
+# Constants defined here
+
+
 class gameWindow:
     def __init__(self):
-        # Define this, the line below is placeholder for the ERROR to shut up
+        # Define this, the line below is placeholder 
         x = 0
     
     def setup(self):
-        # Define this, the line below is placeholder for the ERROR to shut up
+        # Define this, the line below is placeholder 
         x = 0
 
     def on_draw(self):
-        # Define this, the line below is placeholder for the ERROR to shut up
+        # Define this, the line below is placeholder 
         x = 0
 
     def on_key_press(self, key, modifications):
-        # Define this, the line below is placeholder for the ERROR to shut up
+        # Define this, the line below is placeholder 
         x = 0
     
     def on_key_release(self, key, modifications):
-        # Define this, the line below is placeholder for the ERROR to shut up
+        # Define this, the line below is placeholder  
+        x = 0
+        
+    def update(self):
+        # Define this, the line below is placeholder 
         x = 0
     
 # Additional methods for handling data access, title and ending screens, etc. will go here
@@ -29,5 +36,6 @@ def main():
     window.setup()
     arcade.run()
 
+# if main file (not a module) run main
 if __name__ == "__main__":
     main()
