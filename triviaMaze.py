83433da4a142fcd913629ff24655907c97b649fe
Torin Bolander <83034121+GarnelDale@users.noreply.github.com>
2021-05-24 15:20:51 -0400

    Add files via upload
    Initial class/method stub file

---
+++ triviaMaze.py
@@ -0,0 +1,33 @@
+import arcade
+
+class gameWindow:
+    def __init__(self):
+        # Define this, the line below is placeholder for the ERROR to shut up
+        x = 0
+    
+    def setup(self):
+        # Define this, the line below is placeholder for the ERROR to shut up
+        x = 0
+
+    def on_draw(self):
+        # Define this, the line below is placeholder for the ERROR to shut up
+        x = 0
+
+    def on_key_press(self, key, modifications):
+        # Define this, the line below is placeholder for the ERROR to shut up
+        x = 0
+    
+    def on_key_release(self, key, modifications):
+        # Define this, the line below is placeholder for the ERROR to shut up
+        x = 0
+    
+# Additional methods for handling data access, title and ending screens, etc. will go here
+
+def main():
+    # Main method
+    window = gameWindow()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
