9733caa67787e1a1c4187386d3669ac107ddf5b6
Joseph <joseph746567@hotmail.com>
2021-05-27 22:21:25 -0600

    I setup the window being made. the on_update for the callback. The on_draw for drawing the screen.

--- triviaMaze.py
+++ triviaMaze.py
@@ -1,23 +1,30 @@
 import arcade
 
+
 # Constants defined here
 
 
-class gameWindow:
+class gameWindow(arcade.Window):
     def __init__(self):
-        # Define this, the line below is placeholder 
-        x = 0
-    
+        # Call the parent class and set up the window
+        super().__init__(800, 600, "Trivia Maze")
+
+        # The background can be change
+        arcade.set_background_color(arcade.color.AMBER)
+
+
     def setup(self):
-        # Define this, the line below is placeholder 
+        # Define this, the line below is placeholder
         x = 0
+
 
     def on_draw(self):
-        # Define this, the line below is placeholder 
-        x = 0
+        # this will be where all the drawing take place in the game
+        arcade.start_render()
+
 
     def on_key_press(self, key, modifications):
-        # Define this, the line below is placeholder 
+        # Define this, the line below is placeholder
         x = 0
         if key == arcade.key.UP:
             self.up_pressed = True
@@ -27,9 +34,9 @@ class gameWindow:
             self.left_pressed = True
         elif key == arcade.key.RIGHT:
             self.right_pressed = True
-    
+
     def on_key_release(self, key, modifications):
-        # Define this, the line below is placeholder  
+        # Define this, the line below is placeholder
         if key == arcade.key.UP:
             self.up_pressed = False
         elif key == arcade.key.DOWN:
@@ -38,11 +45,13 @@ class gameWindow:
             self.left_pressed = False
         elif key == arcade.key.RIGHT:
             self.right_pressed = False
-        
-    def update(self):
-        # Define this, the line below is placeholder 
+
+
+    def on_update(self, delta_time):
+        # This is the callback. it is set to 60fps by default
         x = 0
-    
+
+
 # Additional methods for handling data access, title and ending screens, etc. will go here
 
 def main():
@@ -50,6 +59,7 @@ def main():
     window = gameWindow()
     window.setup()
     arcade.run()
+
 
 # if main file (not a module) run main
 if __name__ == "__main__":
